cf1b0d40f0bd9187ab4f10eedf2c6affa72ff1ac
Andrew Zito <zitoa@lafayette.edu>
2018-09-18 00:11:34 -0400

    version.php for Moodle 3.5

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018091800;
-$plugin->requires  = 2018091400;
+$plugin->version   = 2018091835;
+$plugin->requires  = 2018051700;
 $plugin->component = 'local_barecrumbs';
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.0.0'; // [major].[minor].[patch]-[Moodle Version].[Moodle version specific patch].
+$plugin->release   = '1.0.0-35.0'; // [major].[minor].[patch]-[Moodle Version].[Moodle version specific patch].
